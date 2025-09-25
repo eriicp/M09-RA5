e83694fe3350706676975683309255d03673e9db
Eric Parada <eric.parada.r@gmail.com>
2025-09-25 19:50:48 +0200

    versio millorada

--- 02-RotX/src/RotX.java
+++ 02-RotX/src/RotX.java
@@ -1,23 +1,20 @@
 public class RotX{
-        public static String minuscules = "aáàbcçdéèfghiíìïjklmnñoóòpqrstuúùüvwxyz";
-        public static String majuscules = "AÁÀBCÇDÉÈFGHIÍÌÏJKLMNÑOÓÒPQRSTUÚÙÜVWXYZ";
-        public static char[] mArray = minuscules.toCharArray();
-        public static char[] MArray = majuscules.toCharArray();
+        final static String minuscules = "aáàbcçdéèfghiíìïjklmnñoóòpqrstuúùüvwxyz";
+        final static String majuscules = "AÁÀBCÇDÉÈFGHIÍÌÏJKLMNÑOÓÒPQRSTUÚÙÜVWXYZ";
+        final static char[] mArray = minuscules.toCharArray();
+        final static char[] MArray = majuscules.toCharArray();
     public static void main(String[] args) {
         int[] numDesplaçat = {0,2,4,6};
         String[] paraulesProba = {"ABC","XYZ","Hola, Mr. calçot","Perdó, per tu què és?"};
-        System.out.println("Xifrat");
-        System.out.println("---------");
+        System.out.println("\nXifrat"+"\n---------");
         for(int i = 0;i < paraulesProba.length;i++){
             System.out.println("("+numDesplaçat[i]+")"+paraulesProba[i] + "         =>" +xifraRotX(paraulesProba[i],numDesplaçat[i]));
         }
-        System.out.println("Desxifrat");
-        System.out.println("---------");
+        System.out.println("\nDesxifrat"+"\n---------");
         for(int i = 0;i < paraulesProba.length;i++){
             System.out.println("("+numDesplaçat[i]+")"+xifraRotX(paraulesProba[i],numDesplaçat[i])+ "         =>" + desxifraRotX(xifraRotX(paraulesProba[i],numDesplaçat[i]),numDesplaçat[i]));
         }
-        System.out.println("Missatge xifrat:" + xifraRotX(paraulesProba[3], numDesplaçat[3]));
-        System.out.println("---------");
+        System.out.println("\nMissatge xifrat:" + xifraRotX(paraulesProba[3], numDesplaçat[3])+"\n---------");
         forcaBrutaRotX(xifraRotX(paraulesProba[3], numDesplaçat[3]));
     }
 
@@ -75,24 +72,7 @@ public static String xifraRotX (String cadena, int desplaçament){
         String resultat = "";
         for (int x = 0; x <= minuscules.length();x++){
             for(int i=0;i < cadenaXifrada.length();i++){
-                char c = cadenaXifrada.charAt(i);
-                if(Character.isLowerCase(c)){
-                    for(int j = 0; j < mArray.length;j++){
-                        if(c == mArray[j]){
-                            c = mArray[(j - x + mArray.length) % mArray.length];
-                            break;
-                        }
-                    }
-                }
-                else{
-                    for(int j = 0; j < MArray.length;j++){
-                        if(c == MArray[j]){
-                            c = MArray[(j - x + MArray.length) % MArray.length];
-                            break;
-                        }
-                    }
-                }
-                resultat += c;
+                resultat = desxifraRotX(cadenaXifrada, x);
             }
             System.out.println("("+x+")->"+resultat);
             resultat = "";
