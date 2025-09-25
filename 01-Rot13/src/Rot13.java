c3963cc0d7cbad0557172c9a03d778d6125dfd58
Eric Parada <eric.parada.r@gmail.com>
2025-09-25 18:41:55 +0200

    25-9

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -1,6 +1,15 @@
 public class Rot13{
     public static void main(String[] args){
-        System.out.println(xifraRot13("b"));
+        System.out.println("Xifrat");
+        System.out.println("---------");
+        for(int i = 0;i < args.length;i++){
+            System.out.println(args[i] + "         =>" +xifraRot13(args[i]));
+        }
+        System.out.println("Desxifrat");
+        System.out.println("---------");
+        for(int i = 0;i < args.length;i++){
+            System.out.println(xifraRot13(args[i])+ "         =>" + desxifraRot13(xifraRot13(args[i])));
+        }
     }
 
     public static String xifraRot13 (String cadena){
@@ -12,17 +21,17 @@ public class Rot13{
         for(int i=0;i < cadena.length();i++){
             char c = cadena.charAt(i);
             if(Character.isLowerCase(c)){
-                for(int j = 0; j < mArray.length;i++){
+                for(int j = 0; j < mArray.length;j++){
                     if(c == mArray[j]){
-                        c = mArray[j + 13];
+                        c = mArray[(j + 13) % mArray.length];
                         break;
                     }
                 }
             }
             else{
-                for(int j = 0; j < MArray.length;i++){
+                for(int j = 0; j < MArray.length;j++){
                     if(c == MArray[j]){
-                        c = MArray[j + 13];
+                        c = MArray[(j + 13) % MArray.length];
                         break;
                     }
                 }
@@ -41,17 +50,17 @@ public class Rot13{
         for(int i=0;i < cadena.length();i++){
             char c = cadena.charAt(i);
             if(Character.isLowerCase(c)){
-                for(int j = 0; j < mArray.length;i++){
+                for(int j = 0; j < mArray.length;j++){
                     if(c == mArray[j]){
-                        c = mArray[j - 13];
+                        c = mArray[(j - 13 + mArray.length) % mArray.length];
                         break;
                     }
                 }
             }
             else{
-                for(int j = 0; j < MArray.length;i++){
+                for(int j = 0; j < MArray.length;j++){
                     if(c == MArray[j]){
-                        c = MArray[j - 13];
+                        c = MArray[(j - 13 + MArray.length) % MArray.length];
                         break;
                     }
                 }
