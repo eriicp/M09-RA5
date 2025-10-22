133a81af8093eacbeb269351432251d23a1bc6d0
Eric Parada <eric.parada.r@gmail.com>
2025-10-22 19:56:43 +0200

    final

--- 05-AES/src/AES.java
+++ 05-AES/src/AES.java
@@ -2,6 +2,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -13,7 +14,6 @@ public class AES {
     public static final String FORMAT_AES = "AES/CBC/PKCS5Padding";
 
     private static final int MIDA_IV = 16;
-    private static byte[] iv = new byte [MIDA_IV];
     private static final String CLAU = "LaClauSecretaQueVulguis";
     
     public static void main(String[] args) {
@@ -22,34 +22,37 @@ public class AES {
                         "Agora Illa Otto"};
 
         for (int i = 0; i < msgs.length; i++) {
-                String msg = msgs[1];
+                String msg = msgs[i]; 
                 
                 byte[] bXifrats = null;
                 String desxifrat = "";
                 try {
-                    bXifrats = xifraAES (msg, CLAU);
+                    bXifrats = xifraAES(msg, CLAU);
                     desxifrat = desxifraAES(bXifrats, CLAU);
                 } catch (Exception e) {
-                System.err.println("Error de xifrat: "
-                    +e.getLocalizedMessage());
+                    System.err.println("Error de xifrat: " + e.getLocalizedMessage());
+                    e.printStackTrace();
                 }            
             System.out.println("");
-            System.out.println("Msg: "+msg);
-            System.out.println("Enc: "+ new String(bXifrats));
-            System.out.println("DEC: + desxifrat");
+            System.out.println("Msg: " + msg);
+            System.out.println("Enc (hex): " + bytesToHex(bXifrats));
+            System.out.println("DEC: " + desxifrat); 
         }
     }
 
-    public static byte[] xifraAES(String msg, String password)throws Exception{
-        byte[] bytesString = msg.getBytes();
+    public static byte[] xifraAES(String msg, String password) throws Exception {
+        byte[] bytesString = msg.getBytes(StandardCharsets.UTF_8);
         
-        IvParameterSpec ivParameterSpec = generaIvParameterSpec();
+        byte[] iv = new byte[MIDA_IV];
+        SecureRandom randomSecureRandom = new SecureRandom();
+        randomSecureRandom.nextBytes(iv);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
         
         MessageDigest digest = MessageDigest.getInstance(ALGORISME_HASH);
         byte[] hashbytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
         SecretKeySpec secretKey = new SecretKeySpec(hashbytes, ALGORISME_XIFRAT);
 
-        Cipher cipher = Cipher.getInstance(ALGORISME_XIFRAT);
+        Cipher cipher = Cipher.getInstance(FORMAT_AES); 
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec);
         byte[] msgXifrat = cipher.doFinal(bytesString);
 
@@ -59,14 +62,27 @@ public class AES {
         return resultat;
     }
 
-    public static IvParameterSpec generaIvParameterSpec() throws NoSuchAlgorithmException{
-        SecureRandom randomSecureRandom = SecureRandom.getInstance(ALGORISME_XIFRAT);
-        randomSecureRandom.nextBytes(iv);
-        IvParameterSpec ivParams = new IvParameterSpec(iv);
-        return ivParams;
+    public static String desxifraAES(byte[] bMsgXifrat, String password) throws Exception {
+        byte[] iv = Arrays.copyOfRange(bMsgXifrat, 0, MIDA_IV);
+        
+        byte[] msgXifrat = Arrays.copyOfRange(bMsgXifrat, MIDA_IV, bMsgXifrat.length);
+        
+        MessageDigest digest = MessageDigest.getInstance(ALGORISME_HASH);
+        byte[] hashbytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+        SecretKeySpec secretKey = new SecretKeySpec(hashbytes, ALGORISME_XIFRAT);
+
+        Cipher cipher = Cipher.getInstance(FORMAT_AES);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+        byte[] msgDesxifrat = cipher.doFinal(msgXifrat);
+        
+        return new String(msgDesxifrat, StandardCharsets.UTF_8);
     }
 
-    public static String desxifraAES(byte[] bMsgXifrat, String password){
-        return "si";
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 }
